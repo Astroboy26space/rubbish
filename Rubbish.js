b3e327e096824528279656c14c6337f69d238592
Astroboy26space <77745423+Astroboy26space@users.noreply.github.com>
2021-07-10 16:59:12 +0100

    Add files via upload

---
+++ Rubbish.js
@@ -0,0 +1,37 @@
+class crumpledpaper
+{
+    constructor(x,y){
+
+
+        this.x=x;
+		this.y=y;
+        var options={
+           isStatic:false,
+           restitution:0.3,
+           friction:0,
+           density:1.2
+         }
+
+         display()
+         {
+
+
+
+
+
+
+            
+         }
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
